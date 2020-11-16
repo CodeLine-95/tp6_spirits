da5d565e54993201b6b602110997e6875abbbd5b
qiaoshuai <13295518684@163.com>
2020-11-16 11:11:13 +0800

    增加自定义错误页面配置

--- config/app.php
+++ config/app.php
@@ -23,14 +23,19 @@ return [
     'domain_bind'      => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list'    => [
-        'common','middleware'
+        'common','middleware','errors'
     ],
 
     // 异常页面的模板文件
-    'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
+    'exception_tmpl'   => app()->isDebug() == true ? app()->getThinkPath() . 'tpl/think_exception.tpl' : base_path() . '/errors/errors.html',
 
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
     'show_error_msg'   => false,
+
+    // 自定义404页面
+    'http_exception_tmpl' => [
+        404  => base_path() . 'errors/404.html',
+    ],
 ];
